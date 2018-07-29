697bcf1269c06aa247c3002a4659356ad27397e4
Alex <alex@stty.io>
2018-07-30 01:50:37 +0800

    Fix indendation and launch X sessions with consolekit
    This is a pulseaudio compat fix as well

--- prepare.sh
+++ prepare.sh
@@ -9,88 +9,88 @@ PCI_BRIDGE=0000:00:01.0
 # @TODO put in a check against whoami or $USER
 
 function perror() {
-	echo "$@" 1>&2;
+  echo "$@" 1>&2;
 }
 
 function bus_rescan() {
-	perror "[*] rescaning pci bus"
-	echo 1 > /sys/bus/pci/rescan
+  perror "[*] rescaning pci bus"
+  echo 1 > /sys/bus/pci/rescan
 }
 
 # usage
 # vfio-bind $GPU vfio-bind
 function driver_bind() {
-	DEV="$1"
-	DRV="$2"
-	VENDOR=$(< /sys/bus/pci/devices/$DEV/vendor)
-	DEVICE=$(< /sys/bus/pci/devices/$DEV/device)
-	if [ -e /sys/bus/pci/devices/$DEV/driver ]; then
-		perror "[*] unbinding $DEV"
-		echo $DEV > /sys/bus/pci/devices/$DEV/driver/unbind
-		sleep .5
-	else
-		perror "[!] existing driver for $DEV not found"
-	fi
-	perror "[*] binding $DEV to $DRV"
-	echo $VENDOR $DEVICE > /sys/bus/pci/drivers/$DRV/new_id
+  DEV="$1"
+  DRV="$2"
+  VENDOR=$(< /sys/bus/pci/devices/$DEV/vendor)
+  DEVICE=$(< /sys/bus/pci/devices/$DEV/device)
+  if [ -e /sys/bus/pci/devices/$DEV/driver ]; then
+    perror "[*] unbinding $DEV"
+    echo $DEV > /sys/bus/pci/devices/$DEV/driver/unbind
+    sleep .5
+  else
+    perror "[!] existing driver for $DEV not found"
+  fi
+  perror "[*] binding $DEV to $DRV"
+  echo $VENDOR $DEVICE > /sys/bus/pci/drivers/$DRV/new_id
 }
 
 function remove_pci_bridge() {
-	perror "[*] removing pci bridge"
-	echo 1 > /sys/bus/pci/devices/$PCI_BRIDGE/remove
-	bus_rescan
+  perror "[*] removing pci bridge"
+  echo 1 > /sys/bus/pci/devices/$PCI_BRIDGE/remove
+  bus_rescan
 }
 
 function unbind_fb_vtconsole() {
-	perror "[*] removing efifb and vtconsole binding"
-	perror "[!!] you WILL lose console"
-	echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/unbind
-	sleep 1
-	echo 0 > /sys/class/vtconsole/vtcon0/bind
-	sleep 1
-	echo 0 > /sys/class/vtconsole/vtcon1/bind
+  perror "[*] removing efifb and vtconsole binding"
+  perror "[!!] you WILL lose console"
+  echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/unbind
+  sleep 1
+  echo 0 > /sys/class/vtconsole/vtcon0/bind
+  sleep 1
+  echo 0 > /sys/class/vtconsole/vtcon1/bind
 }
 
 function rebind_fb_vtconsole() {
-	#echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
-	echo 1 > /sys/class/vtconsole/vtcon0/bind
-	sleep 1
-	echo 1 > /sys/class/vtconsole/vtcon1/bind
+  #echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+  echo 1 > /sys/class/vtconsole/vtcon0/bind
+  sleep 1
+  echo 1 > /sys/class/vtconsole/vtcon1/bind
 }
 
 #
 function intel() {
-	killall X
-	sleep 5
+  killall X
+  sleep 5
 
-	#unbind_fb_vtconsole
-	driver_bind $GPU vfio-pci
-	driver_bind $GPU_SND vfio-pci
-	remove_pci_bridge
+  #unbind_fb_vtconsole
+  driver_bind $GPU vfio-pci
+  driver_bind $GPU_SND vfio-pci
+  remove_pci_bridge
 
-	perror "[*] switching monitor 1 to mDP"
-	ddccontrol -r 0x60 -w 16 dev:/dev/i2c-3
+  perror "[*] switching monitor 1 to mDP"
+  ddccontrol -r 0x60 -w 16 dev:/dev/i2c-3
 
-	touch /tmp/vm.lock
-	su alex -c startx
+  touch /tmp/vm.lock
+  su alex -c 'ck-launch-session dbus-launch --sh-syntax --exit-with-session startx'
 }
 
 function host() {
-	killall X
-	sleep 5
+  killall X
+  sleep 5
 
-	driver_bind $GPU nvidia
-	driver_bind $GPU_SND snd_hda_intel
-	bus_rescan
+  driver_bind $GPU nvidia
+  driver_bind $GPU_SND snd_hda_intel
+  bus_rescan
 
-	rm /tmp/vm.lock
-	ddccontrol -r 0x60 -w 15 dev:/dev/i2c-3
-	#rebind_fb_vtconsole()
-	su alex -c startx
+  rm /tmp/vm.lock
+  ddccontrol -r 0x60 -w 15 dev:/dev/i2c-3
+  #rebind_fb_vtconsole()
+  su alex -c 'ck-launch-session dbus-launch --sh-syntax --exit-with-session startx'
 }
 
 if [ "$1" == "vm" ]; then
-	intel
+  intel
 elif [ "$1" == "host" ]; then
-	host
+  host
 fi
--- run.sh
+++ run.sh
@@ -1,35 +1,19 @@
-export QEMU_AUDIO_DRV=alsa
-export QEMU_ALSA_DAC_SIZE_IN_USEC=0
-export QEMU_ALSA_DAC_BUFFER_SIZE=7526
-export QEMU_ALSA_DAC_PERIOD_SIZE=940
-export QEMU_ALSA_ADC_SIZE_IN_USEC=0
-export QEMU_ALSA_ADC_BUFFER_SIZE=7526
-export QEMU_ALSA_ADC_PERIOD_SIZE=940
-export QEMU_AUDIO_DAC_FIXED_SETTINGS=1
-export QEMU_AUDIO_DAC_FIXED_FREQ=44100
-export QEMU_AUDIO_DAC_FIXED_FMT=S16
-export QEMU_AUDIO_ADC_FIXED_FREQ=44100
-export QEMU_AUDIO_ADC_FIXED_FMT=S16
-export QEMU_AUDIO_DAC_TRY_POLL=0
-export QEMU_AUDIO_ADC_TRY_POLL=0
-export QEMU_AUDIO_TIMER_PERIOD=256
+export QEMU_AUDIO_DRV=pa
 qemu-system-x86_64 -enable-kvm -m 8G \
-    -cpu host,kvm=off,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
-    -rtc clock=host,base=localtime \
-    -smp 4,sockets=1,cores=2,threads=2 \
-    -device vfio-pci,host=01:00.0,multifunction=on,x-vga=on \
-    -device vfio-pci,host=01:00.1 \
-    -drive if=pflash,format=raw,file=/usr/share/edk2-ovmf/OVMF_CODE.fd \
-    -drive media=cdrom,file=win10.iso,id=virtiocd1,if=none \
-    -device ide-cd,bus=ide.1,drive=virtiocd1 \
-    -soundhw ac97 \
-    -vga none \
-    -object input-linux,id=kbd,evdev=/dev/input/by-id/usb-04d9_USB-HID_Keyboard-event-kbd,grab_all=on,repeat=on \
-    -object input-linux,id=mouse,evdev=/dev/input/by-id/usb-Logitech_G102_Prodigy_Gaming_Mouse_017F36743435-event-mouse \
-    -object input-linux,id=kbd2,evdev=/dev/input/by-id/usb-Logitech_G102_Prodigy_Gaming_Mouse_017F36743435-if01-event-kbd,grab_all=on,repeat=on \
-    -drive id=rootfs,file=windoze.img.raw,id=disk,format=raw,if=none \
-    -device virtio-scsi-pci,id=scsi0 \
-    -device scsi-hd,bus=scsi0.0,drive=rootfs \
-    -drive id=gamesfs,file=/qpool/games.img,id=disk,format=qcow2,if=none \
-    -device scsi-hd,bus=scsi0.0,drive=gamesfs \
-    -boot order=dc
+  -cpu host,kvm=off,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
+  -rtc clock=host,base=localtime \
+  -smp 4,sockets=1,cores=2,threads=2 \
+  -device vfio-pci,host=01:00.0,multifunction=on,x-vga=on \
+  -device vfio-pci,host=01:00.1 \
+  -drive if=pflash,format=raw,file=/usr/share/edk2-ovmf/OVMF_CODE.fd \
+  -soundhw ac97 \
+  -vga none \
+  -object input-linux,id=kbd,evdev=/dev/input/by-id/usb-04d9_USB-HID_Keyboard-event-kbd,grab_all=on,repeat=on \
+  -object input-linux,id=mouse,evdev=/dev/input/by-id/usb-Logitech_G102_Prodigy_Gaming_Mouse_017F36743435-event-mouse \
+  -object input-linux,id=kbd2,evdev=/dev/input/by-id/usb-Logitech_G102_Prodigy_Gaming_Mouse_017F36743435-if01-event-kbd,grab_all=on,repeat=on \
+  -drive id=rootfs,file=/home/alex/vfio/windoze.img.raw,id=disk,format=raw,if=none \
+  -device virtio-scsi-pci,id=scsi0 \
+  -device scsi-hd,bus=scsi0.0,drive=rootfs \
+  -drive id=gamesfs,file=/qpool/games.img,id=disk,format=qcow2,if=none \
+  -device scsi-hd,bus=scsi0.0,drive=gamesfs \
+  -boot order=dc
